f27812418f3957b1a0acb68f60acf8b0f05c855f
Adam Filbert <adamfilbert@Adams-MacBook-Pro-2.local>
2018-03-18 20:35:32 -0400

    edited homepage css

--- labor-neighbor-app/src/Components/Body.js
+++ labor-neighbor-app/src/Components/Body.js
@@ -14,12 +14,6 @@ export default class Body extends Component{
 			<div className="component_body">
 				<div className="component_body_intro">
 					<h1> Welcome to Labor Neighbor </h1>
-					<div className="component_body_intro_icons">
-						<img className="component_body_intro_icons_single" src={icon} alt="work icon" />
-						<img className="component_body_intro_icons_single" src={icon} alt="work icon" />
-						<img className="component_body_intro_icons_single" src={icon} alt="work icon" />
-						<img className="component_body_intro_icons_single" src={icon} alt="work icon" />
-					</div>
 				</div>
 				<div className="component_body_about">
 					<h2>Find Small Jobs Faster. Smarter. Easier.</h2>
@@ -49,6 +43,31 @@ export default class Body extends Component{
 				</div>
 				<div className="component_body_tasks">
 					<h2>Tasks</h2>
+					<ul>
+						<li>Task 1</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<ul>
+						<li>Task 4</li>
+						<li>Task 5</li>
+						<li>Task 6</li>
+					</ul>
+					<ul>
+						<li>Task 7</li>
+						<li>Task 8</li>
+						<li>Task 9</li>
+					</ul>
+					<ul>
+						<li>Task 10</li>
+						<li>Task 11</li>
+						<li>Task 12</li>
+					</ul>
+					<ul>
+						<li>Task 13</li>
+						<li>Task 14</li>
+						<li>Task 15</li>
+					</ul>
 				</div>
 				<div className="component_body_whowehelp">
 					<h2>Who We Help</h2>
