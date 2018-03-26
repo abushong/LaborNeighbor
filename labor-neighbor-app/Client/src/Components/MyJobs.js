8a733a15af67ee53689895a7f2aeff9d2e5d0789
abushong <alexbushong@Alexs-MacBook-Pro-2.local>
2018-03-26 11:01:18 -0400

    small changes

--- labor-neighbor-app/Client/src/Components/MyJobs.js
+++ labor-neighbor-app/Client/src/Components/MyJobs.js
@@ -9,7 +9,7 @@ export default class MyJobs extends Component{
 				<div className="component_myjobs_title">
 					<h3>My Jobs</h3>
 					<div className="component_myjobs_break">.</div>
-					<p>You curretnly have no jobs.</p>
+					<p className="component_myjobs_jobs">You curretnly have no jobs.</p>
 				</div>
 			</div>
 		);
