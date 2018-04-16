e0086765c22c15ac316b8eed9ac55164179fc1c1
abushong <alexbushong@Alexs-MacBook-Pro-2.local>
2018-04-16 13:36:03 -0400

    same thing

--- labor-neighbor-app/Client/src/routes.js
+++ labor-neighbor-app/Client/src/routes.js
@@ -4,6 +4,7 @@ import About from "Components/About.js";
 import Body from "Components/Body.js";
 import SignUpSheet from "Components/SignUpSheet.js";
 import JobBoard from "Components/JobBoard.js";
+import Contact from "Components/Contact.js";
 
 export default class Routes extends Component{
 	render() {
@@ -13,6 +14,7 @@ export default class Routes extends Component{
 				<Route path="/signup" component={SignUpSheet} />
 				<Route path="/jobboard" component={JobBoard} />
 				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
 			</Switch>
 		);
 	}
