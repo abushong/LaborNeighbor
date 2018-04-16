8f1c66523cccdac28e8cdbd0f47e979f948347eb
abushong <alexbushong@Alexs-MacBook-Pro-2.local>
2018-04-16 13:17:16 -0400

    fixed contact component

--- labor-neighbor-app/Client/src/Components/Contact.js
+++ labor-neighbor-app/Client/src/Components/Contact.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
-export default class Footer extends Component{
+
+export default class Contact extends Component{
 	render(){
 		return(
 		<div className="component_contact">
-  <form>
+      <form>
 
-    <label for="fname">First Name</label>
-    <input type="text" idName="fname" name="firstname" placeholder="First Name" />
+        <label for="fname">First Name</label>
+        <input type="text" idName="fname" name="firstname" placeholder="First Name" />
 
-    <label for="lname">Last Name</label>
-    <input type="text" idName="lname" name="lastname" placeholder="Last Name" />
+        <label for="lname">Last Name</label>
+        <input type="text" idName="lname" name="lastname" placeholder="Last Name" />
 
-    <label for="country">What can we help you with?</label>
-    <select idName="country" name="country">
-      <option value="general information">General Information</option>
-      <option value="website troubleshooting">Website Troubleshooting</option>
-      <option value="payment problems">Payment Problem</option>
-      <option value="task completion problems">Task Completion Problem</option>
-    </select>
+        <label for="country">What can we help you with?</label>
+        <select idName="country" name="country">
+          <option value="general information">General Information</option>
+          <option value="website troubleshooting">Website Troubleshooting</option>
+          <option value="payment problems">Payment Problem</option>
+          <option value="task completion problems">Task Completion Problem</option>
+        </select>
 
-    <label for="subject">Message</label>
-    <textarea idName="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+        <label for="subject">Message</label>
+        <textarea idName="subject" name="subject" placeholder="Write something.."></textarea>
 
-    <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" />
 
-  </form>
-</div>
+      </form>
+    </div>
 		);
 	}
 }
