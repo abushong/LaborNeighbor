6e46ab68cf17e29771298f843959494836b443ee
Adam Filbert <adamfilbert@Adams-MacBook-Pro-2.local>
2018-04-22 13:55:16 -0400

    blah

--- labor-neighbor-app/Client/src/Components/Header.js
+++ labor-neighbor-app/Client/src/Components/Header.js
@@ -19,21 +19,8 @@ export default class Header extends Component{
 					<p className="component_header_text_right">About</p>
 				</Link>
 			</header>
+
 			
-			<header className="component_header">
-				<Link to="/">
-					<img className="component_header_img" src={logo} alt="logo" />
-				</Link>
-				<Link to="/signup">
-					<p className="component_header_text_right">Sign In</p>
-				</Link>
-				<Link to="/contact">
-					<p className="component_header_text_right">Contact</p>
-				</Link>
-				<Link to="/about">
-					<p className="component_header_text_right">About</p>
-				</Link>
-			</header>
 		);
 	}
 }
