674c3e86fc6a1a136cb04a1aad7b2782e76c85de
abushong <alexbushong@Alexs-MacBook-Pro-2.local>
2018-05-03 21:20:04 -0400

    style

--- labor-neighbor-app/Client/src/Components/Footer.js
+++ labor-neighbor-app/Client/src/Components/Footer.js
@@ -15,8 +15,12 @@ export default class Footer extends Component{
 				<div className="component_footer_text">
 					<p className="component_footer_text">Find Small Jobs. Fast, smarter, easier. </p>
 				 	| <a href="mailto:service@laborneighbor.com">service@laborneighbor.com</a>
+				 	| <p className="component_footer_signout" onClick={() => {this.signOut()}}>sign out</p>
 				 	<br></br>
-				 	<p className="component_footer_signout" onClick={() => {this.signOut()}}>sign out</p>
+				 	<FontAwesome className="component_footer_icon" name='fab fa-facebook' />
+					<FontAwesome className="component_footer_icon" name='far fa-star' />
+					<FontAwesome className="component_footer_icon" name='star' />
+					<FontAwesome className="component_footer_icon" name='far fa-star' />
 				</div>
 			</footer>
 		);
